261ec96c9a1897f474a9fde8b7bf1fe018ed4f4d
Tobias <tobiasoo@stud.ntnu.no>
2025-03-04 21:11:16 +0100

    Set up basic project structure

---
+++ src/test/java/com/ezdevz/gjett_sangen/GjettSangenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezdevz.gjett_sangen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjettSangenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
