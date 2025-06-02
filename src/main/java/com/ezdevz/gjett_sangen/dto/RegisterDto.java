fa608c83f04b2453aeedda216f21e99311743ec5
Tobias <tobiasoo@stud.ntnu.no>
2025-06-02 16:56:26 +0200

    feat: dto for creating a user and repository for users created

--- src/main/java/com/ezdevz/gjett_sangen/dto/RegisterDto.java
+++ src/main/java/com/ezdevz/gjett_sangen/dto/RegisterDto.java
@@ -1,2 +1,27 @@
-package com.ezdevz.gjett_sangen.dto;public class RegisterDto {
+package com.ezdevz.gjett_sangen.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterDto {
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String password;
+
 }
+
--- src/main/java/com/ezdevz/gjett_sangen/repository/UserRepository.java
+++ src/main/java/com/ezdevz/gjett_sangen/repository/UserRepository.java
@@ -1,2 +1,21 @@
-package com.ezdevz.gjett_sangen.repository;public class UserRepository {
+package com.ezdevz.gjett_sangen.repository;
+
+import com.ezdevz.gjett_sangen.model.User;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @NonNull List<User> findAll();
+
+    Optional<User> findByEmail(@NonNull String email);
+
+    Optional<User> findByUsername(@NonNull String username);
+
+    Optional<User> findById(@NonNull Long id);
 }
